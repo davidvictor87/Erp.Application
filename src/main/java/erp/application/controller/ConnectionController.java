493f89fd3f075d0fc54bbb43dd5e538e39fd0b3e
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2019-11-03 11:03:38 +0200

    .

--- src/main/java/erp/application/controller/ConnectionController.java
+++ src/main/java/erp/application/controller/ConnectionController.java
@@ -1,15 +1,7 @@
 package erp.application.controller;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import erp.application.products.ProductsRepository;
 import erp.application.entities.LOG;
-import erp.application.employee.model.Employee;
 import erp.application.employee.model.EmployeeInitialSavedData;
 import erp.application.employee.repository.EmployeeInitialSavedDataRepo;
 import erp.application.entities.CreateFiles;
