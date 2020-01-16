b9b55da2d3c413d7c4f3bb721fce67114e100b9e
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2020-01-16 20:56:17 +0200

    .

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,28 +1,3 @@
-/**
- * 
- */
-
-/*$(document).ready(function(){
-	
-	$('.table .eBtn').on('click', function(event){
-		event.preventDefault();
-		(var href = $(this).attr('href');
-		
-		$.get(href, function(users, status){
-			$('.myForm #id').val(users.id);
-			$('.myForm #email').val(users.email);
-			$('.myForm #password').val(users.password);
-			$('.myForm #fName').val(users.name);
-			$('.myForm #sName').val(users.lastName);
-			$('.myForm #status').val(users.active);
-		});
-		
-		$('.myForm #exampleModal').modal();
-		
-	});
-	
-});*/
-
 $(document).ready(function () {
 
     $('.nBtn, .table .eBtn').on('click', function (event) {
