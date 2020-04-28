37d4f478414111b76404726bdf83625e518c1f7a
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2020-04-28 14:39:55 +0300

    Async upload files service

--- src/main/java/erp/application/service/UploadFileService.java
+++ src/main/java/erp/application/service/UploadFileService.java
@@ -3,31 +3,81 @@ package erp.application.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import org.springframework.stereotype.Service;
+
+import erp.application.entities.LOG;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 @Service
 public class UploadFileService {
 
-	public void uploadFile(String filePath){
-		File receivedFile = null;
-		FileOutputStream fout = null;
-		final String uploadDirectory = "D:/SourceDirectory/";
-		try {
-			receivedFile = new File(uploadDirectory + "" + filePath);
-			if(!receivedFile.exists()) {
-				receivedFile.createNewFile();
+	
+	public void uploadFile(String filePath) throws IOException {
+
+		StringBuffer fileContentBuilder = new StringBuffer();
+		filePath = "D:/NIO1/" + filePath;
+		AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(Paths.get(filePath),
+				StandardOpenOption.READ);
+		CompletionHandler<Integer, ByteBuffer> handler = new CompletionHandler<Integer, ByteBuffer>() {
+			@Override
+			public void completed(Integer result, ByteBuffer attachment) {
+				for (int i = 0; i < attachment.limit(); i++) {
+				
+				}
 			}
-			fout = new FileOutputStream(receivedFile);
-		}catch (IOException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				fout.close();
-				fout.flush();
-			}catch (IOException e) {
-				e.printStackTrace();
+
+			@Override
+			public void failed(Throwable e, ByteBuffer attachment) {
+				throw new IllegalStateException(e.getMessage());
+			}
+		};
+		final int bufferCount = 5;
+		ByteBuffer buffers[] = new ByteBuffer[bufferCount];
+		for (int i = 0; i < bufferCount; i++) {
+			buffers[i] = ByteBuffer.allocate(10);
+			fileChannel.read(buffers[i], i * 10, buffers[i], handler);
+		}
+		for (ByteBuffer byteBuffer : buffers) {
+			for (int i = 0; i < byteBuffer.limit(); i++) {
+				fileContentBuilder.append((char) +byteBuffer.get(i));
 			}
 		}
+		writeAsyncFile(fileContentBuilder.toString());
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static void writeAsyncFile(String input) throws IOException {
+		byte[] byteArray = input.getBytes();
+		ByteBuffer buffer = ByteBuffer.wrap(byteArray);
+		Path path = Paths.get("D:/NIO2/");
+		if (path.toFile().exists()) {
+			path = Paths.get("D:/NIO2/" + (path.toFile().list().length + 1) + ".txt");
+			path.toFile().createNewFile();
+		}
+		AsynchronousFileChannel channel = AsynchronousFileChannel.open(path, StandardOpenOption.WRITE);
+		@SuppressWarnings("unchecked")
+		CompletionHandler<Integer, String> handler = new CompletionHandler() {
+			@Override
+			public void completed(Object result, Object attachment) {
+				LOG.appLogger().warn(attachment + " completed and " + result + " bytes are written.");
+				LOG.appLogger().warn("Execution Time : " + System.currentTimeMillis());
+			}
+
+			@Override
+			public void failed(Throwable exc, Object attachment) {
+				LOG.appLogger().error(attachment + " failed with exception:");
+				exc.printStackTrace();
+			}
+		};
+		channel.write(buffer, 0, "Async Task", handler);
+		channel.close();
+
 	}
 
 }
