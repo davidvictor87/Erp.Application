d9eb97481c1fcd2b1f03a267621cea5bf72479e4
David Victor <David Victor@LAPTOP-VJ7UGB3U>
2020-05-30 17:35:12 +0300

    .

--- src/main/java/erp/application/controller/ProductController.java
+++ src/main/java/erp/application/controller/ProductController.java
@@ -35,6 +35,7 @@ public class ProductController {
 		productsRepository.save(new Products(id, product_category, product_manufacturer, product_name, vat_level, product_code));
 		System.out.println(allProducts());
 		createProductFiles.writeProductFile(productsRepository.findAll());
+	    LOG.appLogger().info("PRODUCT ADDED");
 		return productsRepository.findById(id);
 	}
 
