cde5e8df237e41368b1cf64b96d34cfcf271ffdd
David Victor <David Victor@LAPTOP-VJ7UGB3U>
2020-05-30 17:44:45 +0300

    .

--- src/main/java/erp/application/controller/ConnectionController.java
+++ src/main/java/erp/application/controller/ConnectionController.java
@@ -2,7 +2,6 @@ package erp.application.controller;
 
 import java.io.IOException;
 import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
