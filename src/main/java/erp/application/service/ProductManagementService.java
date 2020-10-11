a7a4d4109627c3914553c2dbf18f0343916023c0
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2020-10-11 14:11:22 +0300

    .

--- src/main/java/erp/application/service/ProductManagementService.java
+++ src/main/java/erp/application/service/ProductManagementService.java
@@ -1,6 +1,8 @@
 package erp.application.service;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -21,16 +23,50 @@ public class ProductManagementService {
 	}
 
 	@Async
-	public CompletableFuture<Products> findUser(int id) {
+	public CompletableFuture<Products> findProduct(int id) {
 		Products product = null;
 		LOG.appLogger().info("FINDING USER WITH ID: " + id);
 		try {
 			product = productRepositoryImp.findById(id);
 			return CompletableFuture.completedFuture(product);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	public void saveProduct(Products product) {
+		LOG.appLogger().info("SAVE PRODUCT");
+		try {
+			CompletableFuture<Void> cf = CompletableFuture.runAsync(() -> productRepositoryImp.save(product));
+			cf.get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateProduct(Products product) {
+		LOG.appLogger().info("UPDATE PRODUCT");
+		try {
+			CompletableFuture.runAsync(() -> productRepositoryImp.update(product)).get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Async
+	public Map<String, Products> returnAllProducts() throws InterruptedException, ExecutionException{
+		LOG.appLogger().info("RETURN ALL PRODUCTS");
+		CompletableFuture<Map<String, Products>> allProducts = CompletableFuture.supplyAsync(() -> {
+			return productRepositoryImp.findAll();
+		});
+		return allProducts.get();
+	}
+	
+	@Async
+	public void deleteProduct(int id) {
+		LOG.appLogger().info("DELETE PRODUCT");
+		productRepositoryImp.delete(id);
 	}
 
 }
