891931f33b8e44f5b6f8354404520a3eb18a3d36
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2021-02-15 11:19:49 +0200

    .

--- src/main/java/erp/application/login/repository/UserRoleRepository.java
+++ src/main/java/erp/application/login/repository/UserRoleRepository.java
@@ -1,7 +1,13 @@
 package erp.application.login.repository;
 
 import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import erp.application.login.model.UserRole;
 
@@ -11,5 +17,10 @@ public interface UserRoleRepository extends JpaRepository<UserRole, String>{
 	public void deleteByUserId(String id);
 	
 	public CopyOnWriteArrayList<UserRole> findByUserId(String role_ID);
+	
+	@Transactional
+	@Modifying
+	@Query(value="UPDATE role r SET r.role_id= :role_id WHERE r.user_id= :user_id", nativeQuery= true)
+	public void updateUserWithId(@Param(value="user_id") String userId, @Param(value="role_id")String userRole);
 
 }
