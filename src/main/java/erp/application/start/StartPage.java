b7fc2b28b3d78a8c81094bd719724afadc351a55
David Victor <David Victor@LAPTOP-VJ7UGB3U.local>
2021-04-06 13:11:52 +0300

    Import Post Mapping

--- src/main/java/erp/application/start/StartPage.java
+++ src/main/java/erp/application/start/StartPage.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.bind.annotation.RequestMethod;
