9015e49ca44f97f406b981f0b0fae608541c62dd
davidvictor87 <56070889+davidvictor87@users.noreply.github.com>
2021-08-15 14:42:02 +0300

    UpdateDailyTasks

--- src/main/java/erp/application/controller/DailyTasks.java
+++ src/main/java/erp/application/controller/DailyTasks.java
@@ -3,13 +3,20 @@ package erp.application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 
+import erp.application.entities.ApplicationStaticInfo;
 import erp.application.entities.LOG;
+import erp.application.entities.tasks.FileStorage;
 import erp.application.service.EmployeeTimeLogFilesFactory;
 import erp.application.service.FileStorageImpl;
 
@@ -48,7 +55,7 @@ public class DailyTasks {
 				model.addAttribute("name", n);
 				model.addAttribute("appName", environment.getProperty("spring.application.name"));
 				model.addAttribute("files", storageService.listAll().map(
-                      path -> MvcUriComponentsBuilder.fromMethodName(DailyTasks.class, "serveFile",
+                      path -> MvcUriComponentsBuilder.fromMethodName(DailyTasks.class, "deliverFile",
                     		  path.getFileName().toString()).build().toUri().toString())
 						      .collect(Collectors.toSet()));
 				employeeLogFiles.employeeTimeCounter(employeeAuth);
@@ -58,5 +65,19 @@ public class DailyTasks {
 		}
 		return "logged.html";
 	}
-
+	
+	
+	@GetMapping("/deliver/{file}")
+	@ResponseBody
+	public ResponseEntity<?> deliverFile(@PathVariable String file){
+		LOG.appLogger().info(" === Deliver File ===");
+		try {
+			file = ApplicationStaticInfo.FOLDER__TO_ITERATE;
+			Resource resource = storageService.loadResource(file);
+			return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, file).body(resource);
+		}catch (Exception e) {
+			LOG.appLogger().error("=== ERROR ===", e.fillInStackTrace());
+			throw new RuntimeException();
+		}
+	}
 }
