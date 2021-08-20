3bc4abc3b7342de84c00622a2c52418e856baf85
davidvictor87 <56070889+davidvictor87@users.noreply.github.com>
2021-08-20 12:50:24 +0300

    Enable Async

--- src/main/java/erp/application/entities/tasks/FileStorage.java
+++ src/main/java/erp/application/entities/tasks/FileStorage.java
@@ -1,7 +1,11 @@
 package erp.application.entities.tasks;
 
 import java.nio.file.Path;
+import java.util.concurrent.Future;
 import java.util.stream.Stream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.scheduling.annotation.Async;
 
 public interface FileStorage {
 	
@@ -10,5 +14,8 @@ public interface FileStorage {
 	public Resource loadResource(String file);
 	
 	public void deleteFilesFromWorkingDirectory();
+	
+	@Async
+	public Future<String> editFile(String pathToFile, String editData);
 
 }
