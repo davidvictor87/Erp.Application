41b0f16009444fc60d796d82c76da3e164b6580b
davidvictor87 <56070889+davidvictor87@users.noreply.github.com>
2021-09-24 15:14:46 +0300

    refactoring h2 connection

--- src/main/java/erp/application/h2/model/H2Model.java
+++ src/main/java/erp/application/h2/model/H2Model.java
@@ -2,30 +2,47 @@ package erp.application.h2.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(catalog="testdb", name="test")
+@Table(catalog="test", name="test_users")
 public class H2Model implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	private int id;
+	@Column
+	private Integer id;
+	@Column
 	private String name;
+	@Column
 	private String profession;
+	@Column
 	private String salary;
+	@Column
 	private boolean isEnabled;
 	
 	public H2Model() {}
+	
+	public H2Model(Integer id, String name, String profession, String salary, boolean isEnabled) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.profession = profession;
+		this.salary = salary;
+		this.isEnabled = isEnabled;
+	}
 
-	public int getId() {
+
+
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -59,15 +76,12 @@ public class H2Model implements Serializable{
 
 	public void setEnabled(boolean isEnabled) {
 		this.isEnabled = isEnabled;
-	}
+	}	
 
 	@Override
 	public String toString() {
 		return "H2Model [id=" + id + ", name=" + name + ", profession=" + profession + ", salary=" + salary
 				+ ", isEnabled=" + isEnabled + "]";
 	}
-	
-	
-	
 	
 }
