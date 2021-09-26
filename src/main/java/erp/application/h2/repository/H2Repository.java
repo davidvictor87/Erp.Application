f0724dde21b0bb0a02a6a6573f9a86d69e27f2fe
davidvictor87 <56070889+davidvictor87@users.noreply.github.com>
2021-09-26 19:35:54 +0300

    refactoring h2 connection

--- src/main/java/erp/application/h2/repository/H2Repository.java
+++ src/main/java/erp/application/h2/repository/H2Repository.java
@@ -1,12 +1,12 @@
 package erp.application.h2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
+import javax.transaction.Transactional;
 
 import erp.application.h2.model.H2Model;
 
-@NoRepositoryBean
+@Transactional
 @Repository
 public interface H2Repository extends JpaRepository<H2Model, Integer>{
 	
